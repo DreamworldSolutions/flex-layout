ae3df365c05a7acfa6e60507c58172553b2e7b44
Chandresh Kikani <chandresh.kikani@dreamworldsolutions.com>
2019-07-10 16:52:42 +0530

    Misc typo in exports factors module from index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 export { flexLayout } from './flex-layout';
 export { alignment } from './flex-layout-alignment';
-export { factors } from './flex-layout-alignment';
+export { factors } from './flex-layout-factors';
 export { positioning } from './flex-layout-positioning';
 export { reverse } from './flex-layout-reverse';
